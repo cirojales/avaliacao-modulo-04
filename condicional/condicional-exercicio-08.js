8c70ba12d85ba28ef08b9b4815ff16abe16a8240
Ciro <cbjales@gmail.com>
2022-09-20 20:04:39 -0300

    Solução

--- condicional/condicional-exercicio-08.js
+++ condicional/condicional-exercicio-08.js
@@ -3,7 +3,7 @@
 /* Apliquei o algoritmo de bubble sort: cada elemento é comparado ao elemento seguinte. Se aquele é menor que este, permutam-se os elementos via desestruturação. */
 
 function ordemDescrescente(num1, num2, num3) {
-  let nums = [...arguments]; // spreading do objeto arguments
+  let nums = [...arguments];
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (nums[j] < nums[j + 1]) {
